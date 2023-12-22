5876a2ee17d79c330a5b6a7840d70c907fdf99a1
cloudtechmasters <68885738+cloudtechmasters@users.noreply.github.com>
2023-12-22 14:26:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def index():
     return render_template('index.html', clients_data=clients_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
